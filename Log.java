4d74f0391f047c7de9486be640a6a2c93adba4f0
Pedro Lamkowski <pedro.lamkowski@unesp.br>
2019-04-12 18:01:00 -0300

    Arquivos limpados

--- Log.java
+++
@@ -1,9 +0,0 @@
-public class Log {
-	public static double log(double valor, double base) {
-		return Math.log10(valor) / Math.log10(base);
-	}
-
-	public static void main(String[] args) {
-		System.out.println(log((double) 3, (double) 2));
-	}
-}
